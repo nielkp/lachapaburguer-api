f5305b608d3ae4ca0c0deca940cd9b736d4ad13d
Daniel Knaip <danielknaip@gmail.com>
2025-08-01 19:58:05 -0300

    Criado Controller de Usuário!

---
+++ src/app/controllers/Regras.js
@@ -0,0 +1,7 @@
+//////////////////////////////////////////
+///   store => Cadastrar / Adicionar   ///
+///   index => Listar Vários           ///
+///   show => Listar Apenas Um         ///
+///   update => Atualizar              ///
+///   delete => deletar                ///
+//////////////////////////////////////////
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
-import { Router } from "express";
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get("/status", (request, response) => {
-  return response.status(200).json({ message: "✅ API ⚙️ FUNCIONANDO!!! 🍔🚀" });
+routes.post('/users', UserController.store);
+
+routes.get('/status', (request, response) => {
+  return response
+    .status(200)
+    .json({ message: '✅ API ⚙️ FUNCIONANDO!!! 🍔🚀' });
 });
 
 export default routes;
