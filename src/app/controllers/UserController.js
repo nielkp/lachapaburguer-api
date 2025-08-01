ac611e0546e8ec89779f53f48d6ce6b8c51b2160
Daniel Knaip <danielknaip@gmail.com>
2025-08-01 20:30:59 -0300

    Validação de Dados com YUP!

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,9 +1,29 @@
 import { v4 } from 'uuid';
+import * as Yup from 'yup';
 
 import User from '../models/User';
 
 class UserController {
   async store(request, response) {
+    const schema = Yup.object({
+      name: Yup.string()
+        .strict()
+        .typeError('O nome precisar ser letras')
+        .min(2, 'O nome precisa ter no minimo 2 caracteres')
+        .required('O nome é obrigatório'),
+      email: Yup.string().email('O email não é válido').required(),
+      password_hash: Yup.string()
+        .min(6, 'A senha precisa ter no minimo 6 caracteres')
+        .required(),
+      admin: Yup.boolean(),
+    });
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
     const { name, email, password_hash, admin } = request.body;
 
     const user = await User.create({
