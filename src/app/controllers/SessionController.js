73d8c7478698ff60fc1eb916253dc65bb4feeaeb
Daniel Knaip <danielknaip@gmail.com>
2025-08-01 22:51:48 -0300

    Fazendo Login do Usuário!

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,48 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+
+class SessionController {
+  async store(request, response) {
+    const schema = Yup.object({
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
+    });
+
+    const isValid = await schema.isValid(request.body);
+
+    const emailOrPasswordIncorrect = () => {
+      return response.status(401).json({ error: 'Senha ou email incorretos!' });
+    };
+
+    if (!isValid) {
+      return emailOrPasswordIncorrect();
+    }
+
+    const { email, password } = request.body;
+
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return emailOrPasswordIncorrect();
+    }
+
+    const isSamePassword = await user.checkPassword(password);
+
+    if (!isSamePassword) {
+      return emailOrPasswordIncorrect();
+    }
+
+    return response.status(201).json({
+      id: user.id,
+      name: user.name,
+      email,
+      admin: user.admin,
+    });
+  }
+}
+
+export default new SessionController();
